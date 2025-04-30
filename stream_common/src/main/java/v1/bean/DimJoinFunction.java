f2b76f4619a868e62867241f786903c34ad23540
殷实 <14643122+abundant-and-solid@user.noreply.gitee.com>
2025-04-30 10:36:15 +0800

    250430

---
+++ stream_common/src/main/java/v1/bean/DimJoinFunction.java
@@ -0,0 +1,18 @@
+package v1.bean;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * @Package com.lzy.retail.com.lzy.stream.realtime.v1.realtime.bean.DimJoinFunction
+ * @Author zheyuan.liu
+ * @Date 2025/4/8 8:46
+ * @description: DimJoinFunction
+ */
+
+public interface DimJoinFunction<T> {
+    void addDims(T obj, JSONObject dimJsonObj) ;
+
+    String getTableName() ;
+
+    String getRowKey(T obj) ;
+}
---
+++ stream_common/src/main/java/v1/bean/TrafficUvCt.java
@@ -0,0 +1,20 @@
+package v1.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @Package com.lzy.retail.com.lzy.stream.realtime.v1.realtime.bean.TrafficUvCt
+ * @Author zheyuan.liu
+ * @Date 2025/4/8 13:56
+ * @description: TrafficUvCt
+ */
+
+@Data
+@AllArgsConstructor
+public class TrafficUvCt {
+    // 渠道
+    String ch;
+    // 独立访客数
+    Integer uvCt;
+}
